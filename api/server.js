bb0780178dbec2f4db6c11a2f658114556e69026
Pavan Kumar <pkumar07071990@gmail.com>
2023-06-28 01:07:16 +0530

    shuffled

--- api/server.js
+++ api/server.js
@@ -11,8 +11,31 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("combined"));
 
+const shuffleArray = (arr) => {
+  let currentIndex = arr.length;
+
+  // performance-wise the Fisher-Yates algorithm is much better
+  while (currentIndex > 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [arr[currentIndex], arr[randomIndex]] = [
+      arr[randomIndex],
+      arr[currentIndex],
+    ];
+  }
+
+  return arr;
+};
+
 app.get("/getquestions", (req, res) => {
-  res.send(quetions);
+  try {
+    const { count } = req.query;
+    const slicedArrar = shuffleArray(quetions).slice(0, count);
+    res.status(200).send({ data: slicedArrar, message: "Success" });
+  } catch (err) {
+    res.status(404).send({ data: [], message: "Items failed to load" });
+  }
 });
 
 app.listen(port, () => {
